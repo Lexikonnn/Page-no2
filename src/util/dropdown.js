cf6143aebc908dcca314a02312851da6b5b9e669
skrzeczek <h.skrzeczek@gmail.com>
2025-03-17 17:17:53 +0100

    frontend: hamburger menu added

--- src/util/dropdown.js
+++ src/util/dropdown.js
@@ -1,28 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".dropdown > a").forEach((dropdown) => {
-        dropdown.addEventListener("click", function (e) {
-            e.preventDefault();
-            let menu = this.nextElementSibling;
 
-            // Zavři všechny ostatní dropdowny, pokud kliknu jinam
-            document.querySelectorAll(".dropdown-menu").forEach((otherMenu) => {
-                if (otherMenu !== menu) {
-                    otherMenu.style.opacity = "0";
-                    otherMenu.style.visibility = "hidden";
-                    otherMenu.style.transform = "translateY(10px)";
-                }
-            });
+    const menuToggle = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav-links");
 
-            // Přepnutí viditelnosti dropdownu
-            if (menu.style.opacity === "1") {
-                menu.style.opacity = "0";
-                menu.style.visibility = "hidden";
-                menu.style.transform = "translateY(10px)";
-            } else {
-                menu.style.opacity = "1";
-                menu.style.visibility = "visible";
-                menu.style.transform = "translateY(0)";
-            }
+
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener("click", () => {
+            navLinks.classList.toggle("active");
         });
-    });
+    } else {
+        console.error("menuToggle nebo navLinks nebyly nalezeny!");
+    }
 });
